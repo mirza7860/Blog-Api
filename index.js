82e8395b6b7069efcc3d245dbb2c69184459f3a7
Mirza Sahil Alli Baig <136656845+mirza7860@users.noreply.github.com>
2024-01-27 15:10:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ const secretKey = process.env.JWT_SECRET;
 const __dirname=path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 8000;
-app.use(cors({ credentials: true}));
+app.use(cors({
+  origin: process.env.CLIENT_URL', 
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
