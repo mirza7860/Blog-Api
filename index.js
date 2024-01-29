84bf0c28554c3a0a4f142e4e97069c75a0dac372
Mirza Sahil Alli Baig <136656845+mirza7860@users.noreply.github.com>
2024-01-29 23:31:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Origin", allowedOrigins[0]);
   }
 
   res.header("Access-Control-Allow-Credentials", true);
